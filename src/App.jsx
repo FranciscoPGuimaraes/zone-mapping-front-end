6ebdfdc35064f640093c8932d915e7e35ee13c69
Francisco Guimarães <franciscop.guimaraes04@gmail.com>
2025-11-23 22:41:48 -0300

    feat: add modal and zones

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
+import MapView from "./components/map/MapView";
+import SideBar from "./components/sidebar/SideBar";
+import ZoneModal from "./components/zoneModal/zoneModal";
+import { ZonesProvider } from "./context/ZonesContext";
 import './App.css'
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleSave = (zone) => {
+    console.log("Nova zona:", zone);
+    setOpen(false);
+  };
+
+  const newZone = () => {
+    setOpen(true);
+  }
+
   return (
-    <>
-      <p>
-        
-      </p>
-    </>
-  )
+    <ZonesProvider>
+      <div
+        style={{
+          display: "flex",
+          width: "100vw",
+          height: "100vh",
+          overflow: "visible",
+        }}
+      >
+        <SideBar />
+        <div style={{ flex: 1 }}>
+          <MapView onCreateNewZone={newZone} />
+          <ZoneModal
+            isOpen={open}
+            onClose={() => setOpen(false)}
+            onSave={handleSave}
+          />
+        </div>
+      </div>
+    </ZonesProvider>
+  );
 }
 
 export default App
--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,16 +1,43 @@
-import { styles } from "./style";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, FeatureGroup, GeoJSON } from "react-leaflet";
+import DrawControls from "../drawControls/drawControls"
 import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
+import { useZones } from "../../hooks/useZones";
+import { useRef } from "react";
+import { useZonesContext } from "../../context/ZonesContext";
 
+export default function MapView({onCreateNewZone}) {
+    const { zones } = useZones();
+    const { visibleZones } = useZonesContext();
 
-export default function MapView() {
+    const savedLayersRef = useRef();
+    const drawingLayersRef = useRef();
+
+    function handleZoneCreated(geojson) {
+        onCreateNewZone();
+        console.log("Nova zona criada:", geojson);
+    }
+
     return (
         <MapContainer
-            center={[-23.55, -46.63]}
-            zoom={13}
-            style={styles.MapContainer}
+            center={[-15.5, -47.5]}
+            zoom={5}
+            style={{ height: "100%", width: "100%" }}
         >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+            <FeatureGroup ref={savedLayersRef}>
+                {zones
+                    .filter((zone) => visibleZones[zone.id])
+                    .map((zone) => (
+                        <GeoJSON key={zone.id} data={zone.geometry} />
+                    ))}
+
+            </FeatureGroup>
+
+            <FeatureGroup ref={drawingLayersRef}>
+                <DrawControls onCreated={handleZoneCreated} />
+            </FeatureGroup>
         </MapContainer>
     );
 }
