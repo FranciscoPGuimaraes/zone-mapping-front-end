dd700b4b084c524606dd7483cdc0e4bec1624683
Francisco Guimarães <franciscop.guimaraes04@gmail.com>
2025-11-26 21:21:55 -0300

    feat: refactor zone API functions to use axios for data fetching

--- src/api/zones.js
+++ src/api/zones.js
@@ -4,95 +4,27 @@ const api = axios.create({
     baseURL: "http://localhost:3000",
 });
 
-export const getZones = async () => {
-    const zones = [
-        {
-            id: 1,
-            name: "Zona Residencial Norte",
-            type: "Residencial",
-            geometry: {
-                type: "Feature",
-                geometry: {
-                    type: "Polygon",
-                    coordinates: [
-                        [
-                            [-45.807120, -22.247011],
-                            [-45.707117, -22.246419],
-                            [-45.706941, -22.246419],
-                            [-45.706941, -22.247011],
-                            [-45.707117, -22.247011]
-                        ]
-                    ]
-                },
-                properties: {},
-            }
-        },
-                {
-            id: 2,
-            name: "Zona Residencial Oeste",
-            type: "Residencial",
-            geometry: {
-                type: "Feature",
-                geometry: {
-                    type: "Polygon",
-                    coordinates: [
-                        [
-                            [-45.807120, -22.247011],
-                            [-45.707117, -22.246419],
-                            [-45.706941, -22.246419],
-                            [-45.706941, -22.247011],
-                            [-45.707117, -22.247011]
-                        ]
-                    ]
-                },
-                properties: {},
-            }
-        },
-                {
-            id: 3,
-            name: "Zona Residencial Sul",
-            type: "Residencial",
-            geometry: {
-                type: "Feature",
-                geometry: {
-                    type: "Polygon",
-                    coordinates: [
-                        [
-                            [-45.807120, -22.247011],
-                            [-45.707117, -22.246419],
-                            [-45.706941, -22.246419],
-                            [-45.706941, -22.247011],
-                            [-45.707117, -22.247011]
-                        ]
-                    ]
-                },
-                properties: {},
-            }
-        },
-                {
-            id: 4,
-            name: "Zona Residencial Lest",
-            type: "Residencial",
-            geometry: {
-                type: "Feature",
-                geometry: {
-                    type: "Polygon",
-                    coordinates: [
-                        [
-                            [-45.807120, -22.247011],
-                            [-45.707117, -22.246419],
-                            [-45.706941, -22.246419],
-                            [-45.706941, -22.247011],
-                            [-45.707117, -22.247011]
-                        ]
-                    ]
-                },
-                properties: {},
-            }
-        }
-    ];
-
-    return { data: zones };
+export const listZones = async () => {
+    const response = await api.get("/zones");
+    return response.data;
 };
 
-export const createZone = (data) => api.post("/zones", data);
+export const getZone = async (id) => {
+    const response = await api.get(`/zones/${id}`);
+    return response.data;
+};
+
+export const createZone = async (zone) => {
+    const response = await api.post("/zones", zone);
+    return response.data;
+};
+
+export const updateZone = async (id, zone) => {
+    const response = await api.patch(`/zones/${id}`, zone);
+    return response.data;
+};
+
+export const deleteZone = async (id) => {
+    await api.delete(`/zones/${id}`);
+    return { success: true };
+};
